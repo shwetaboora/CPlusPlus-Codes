b4631c3091d01d94ba7a071ccae714478da8497d
shwetaboora <shwetabooraa@gmail.com>
2024-08-19 20:20:26 +0530

    Stack balanced parenthesis, reverse stack recursion

---
+++ Stacks/balancedParenthesis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+bool isValid(char *s)
+{
+}
+
+int main()
+{
+    char *s = "((a+b)+(C-d+f))";
+
+    return 0;
+}
--- Stacks/recursionReverseStack.cpp
+++ Stacks/recursionReverseStack.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include <stack>
 using namespace std;
+
+void insertAtBottom(stack<int> &s, int x)
+{
+    // base case
+    if (s.empty())
+    {
+        s.push(x);
+        return;
+    }
+    // recursive case
+    int data = s.top();
+    s.pop();
+    insertAtBottom(s, x);
+    s.push(data);
+}
+
+void reverseStack(stack<int> &s)
+{
+    // base case
+    if (s.empty())
+    {
+        return;
+    }
+    // pop out the top element and inset it at the bottom of "reversed smaller stack"
+    int x = s.top();
+    s.pop();
+    // recursively reverse the smaller trek
+    reverseStack(s);
+    insertAtBottom(s, x);
+}
 
 int main()
 {
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    reverseStack(s);
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
 
     return 0;
 }
