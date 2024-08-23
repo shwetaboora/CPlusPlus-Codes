c782eab45a2f5a726d50d39b049f6cb153da7368
shwetaboora <shwetabooraa@gmail.com>
2024-08-23 22:06:34 +0530

    Queue intro

---
+++ Queues/introQueue.cpp
@@ -0,0 +1,7 @@
+// Queue - ATM, Bank, Tickets
+// it is a linear data structure that follows the FIFO ordering
+// insertion of data at the end of queue - Push()
+// deletion of data at the front of the queue = Pop()
+// knowing the front element give data at the front - Front()
+
+// used in operating system , trees, graphs
