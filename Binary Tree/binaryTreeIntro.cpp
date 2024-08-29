74ab6c5094186dfceb3d77d1177f71d74f54ba49
shwetaboora <shwetabooraa@gmail.com>
2024-08-30 02:58:50 +0530

    Binary tree intro pre, post, in, levelorder traversal

---
+++ Binary Tree/binaryTreeIntro.cpp
@@ -0,0 +1,27 @@
+// Binary Tree
+// Non linear or the hierarchical
+// common operations - insertion , deletion, searching, ordering
+// eg - my computer -> cdrive, d drive etc - > they goes to folders in them nd more branches in them
+// in linked list each node is connected to one child
+// binary means two so every node will have atmost two children
+
+// first node is the root node (parent)
+// and branches are children
+
+// ancestors, decendents
+// all the parents of a node are ancestors
+
+// all the nodes derived from the parent are decendents
+
+// leaf nodes are the nodes which do not have any child
+
+// parent node or root node will be --> node (datatype of binary tree)
+// children will be --> node*
+
+// traversal types
+// preorder, inorder, postorder, levelorder
+
+// preorder -> root then left child then right child
+// inorder -> left then root then right
+// postorder(bottom up traversal)-> left then right then root
+// levelorder -> at first level the parent then second level children left and right then third level and continue
