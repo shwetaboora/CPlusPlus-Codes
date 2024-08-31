597c7699c6da4a00c5165e2690bd764dd010f6bc
shwetaboora <shwetabooraa@gmail.com>
2024-08-31 19:00:10 +0530

    Binary tree bfs, levelorder problems

---
+++ Binary Tree/levelOrderTraversal2.cpp
@@ -0,0 +1,86 @@
+// breadth first search traversal
+
+// levelorder print it level wise
+// max level = height
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node *buildTree()
+{
+    int d;
+    cin >> d;
+
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void BFS(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *f = q.front();
+        if (f == NULL)
+        {
+            cout << endl;
+            q.pop();
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+
+            cout << f->data << " ";
+            q.pop();
+            if (f->left)
+            {
+                q.push(f->left);
+            }
+            if (f->right)
+            {
+                q.push(f->right);
+            }
+        }
+    }
+    return;
+}
+
+// 8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+int main()
+{
+    node *root = buildTree();
+
+    BFS(root);
+
+    return 0;
+}
+
+// worst case complexity = O(N^2)
+// O(N) - using BFS
